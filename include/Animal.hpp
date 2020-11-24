10ff31e030302b7cf6dc7c90526ad92ac9c0adb1
DAWERTON EDUARDO CARLOS VAZ <edwardvaz@ufrn.edu.br>
2020-11-24 17:51:01 -0300

    arquivos de cabeçalho

---
+++ include/Animal.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <string>
+using namespace std;
+
+class Animal
+{
+protected:
+    char sexo;
+    double tamanho;
+    string nome;
+    string nome_cientifico;
+    string alimentacao;
+
+public:
+    char get_sexo();
+    void set_sexo(char _sexo);
+
+    double get_tamanho();
+    void set_tamanho(double _tamanho);
+
+    string get_nome();
+    void set_nome(string _nome);
+
+    string get_nome_cientifico();
+    void set_nome_cientifico(string _nome_cientifico);
+
+    string get_alimentacao();
+    void set_alimentacao(string _alimentacao);
+};
+
+class AnimalSilvestre
+{
+protected:
+
+public:
+    AnimalSilvestre();
+    ~AnimalSilvestre();
+};
+
+class AnimalNativo : protected AnimalSilvestre
+{
+protected:
+
+public:
+    AnimalNativo();
+    ~AnimalNativo();
+};
+
+class AnimalExotico : protected AnimalSilvestre
+{
+protected:
+
+public:
+    AnimalExotico();
+    ~AnimalExotico();
+};
