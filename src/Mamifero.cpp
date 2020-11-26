3ff0e4daea9f5ae9e0349123cba6cc2186fb1acf
DAWERTON EDUARDO CARLOS VAZ <edwardvaz@ufrn.edu.br>
2020-11-25 22:06:30 -0300

    Classes: implementação dos construtores

--- src/Mamifero.cpp
+++ src/Mamifero.cpp
@@ -1,7 +1,9 @@
 #include "Mamifero.hpp"
 
 Mamifero::Mamifero(char sexo, double tamanho, double peso, string nome, string nome_cientifico, string alimentacao, bool extinto,string cor_da_pelagem,bool glandulas_sudoriparas):
-                Animal(sexo,tamanho,peso,nome,nome_cientifico,alimentacao,extinto,cor_da_pelagem,glandulas_sudoriparas) {}
+                Animal(sexo,tamanho,peso,nome,nome_cientifico,alimentacao,extinto), 
+                cor_da_pelagem(cor_da_pelagem), 
+                glandulas_sudoriparas(glandulas_sudoriparas) {}
 
 Mamifero::~Mamifero() {}
 
--- src/Reptil.cpp
+++ src/Reptil.cpp
@@ -1,14 +1,16 @@
 #include "Reptil.hpp"
+#include <iomanip>
+#include <iostream>
 
-Reptil::Reptil(char sexo, double tamanho, double peso, string nome, string nome_cientifico, string alimentacao, bool extinto,bool peconheto,string tipo_veneno):
-                Animal(sexo,tamanho,peso,nome,nome_cientifico,alimentacao,extinto,peconheto,tipo_veneno) {}
+Reptil::Reptil(char sexo, double tamanho, double peso, string nome, string nome_cientifico, string alimentacao, bool extinto, bool peconheto, string tipo_veneno) : 
+            Animal(sexo, tamanho, peso, nome, nome_cientifico, alimentacao, extinto),
+            peconheto(peconheto), 
+            tipo_veneno(tipo_veneno) {}
 
 Reptil::~Reptil() {}
 
-
-ostream& Reptil::imprimeDados(ostream& o) const{
-    //corpo
+ostream &Reptil::imprimeDados(ostream &o) const
+{
 
     return o;
 }
-
