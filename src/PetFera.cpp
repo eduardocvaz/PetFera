19c3155c2c4464c2e3f41b29729d9c053266d15b
Leandro Junior <leandro.ferreira.jr@gmail.com>
2020-11-26 14:29:35 -0400

    Implementação do construtor, destrutor e métodos get.

--- src/PetFera.cpp
+++ src/PetFera.cpp
@@ -1,2 +1,46 @@
 #include "PetFera.h"
 
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <cstring>
+
+PetFera::PetFera(string telefone, string endereco):
+                telefone(telefone), endereco(endereco) {}
+
+PetFera::~PetFera() {
+    for (auto& animal : this->animais)
+    {
+        delete animal;
+    }
+
+    for (auto& profissional : this->profissionais)
+    {
+        delete profissional;
+    }
+}
+
+string PetFera::getTelefone() const {
+    return this->telefone;
+}
+
+string PetFera::getEndereco() const {
+    return this->endereco;
+}
+
+vector<Animal*> PetFera::getAnimais() const{
+    return this->animais;
+}
+
+vector<Profissional*> PetFera::getProfissionais() const{
+    return this->profissionais;
+}
+
+
+
+
+
+
+
+
